1b9c2c41a3793061d672f35c6c96d002745ec666
undnull <41549137+undnull@users.noreply.github.com>
2021-03-10 13:08:38 +0500

    Add some performance improvements. Instead of generating the transformation matrix every time we change something in the container we can generate it on demand once.

--- include/data/transform.hh
+++ include/data/transform.hh
@@ -26,17 +26,13 @@ public:
     float getRotation() const;
     float getScale() const;
 
-    void update();
-
-    const float4x4_t &getMatrix() const;
+    const float4x4_t getMatrix() const;
 
 private:
     float2_t origin;
     float2_t position;
     float rotation;
     float scale_v;
-
-    float4x4_t matrix;
 };
 
 inline Transform::Transform()
@@ -45,52 +41,42 @@ inline Transform::Transform()
     position = float2_t(0.0f, 0.0f);
     rotation = 0.0f;
     scale_v = 1.0f;
-
-    matrix = float4x4_t(1.0f);
-    update();
 }
 
 inline void Transform::setOrigin(const float2_t &origin)
 {
     this->origin = origin;
-    update();
 }
 
 inline void Transform::setPosition(const float2_t &position)
 {
     this->position = position;
-    update();
 }
 
 inline void Transform::setRotation(float rotation)
 {
     this->rotation = rotation;
-    update();
 }
 
 inline void Transform::setScale(float scale_v)
 {
     this->scale_v = scale_v;
-    update();
 }
 
 inline void Transform::move(const float2_t &velocity)
 {
     position += velocity;
-    update();
 }
 
 inline void Transform::rotate(float angle)
 {
     rotation += angle;
     rotation = fmodf(rotation, 360.0f);
-    update();
 }
 
 inline void Transform::scale(float f)
 {
     scale_v *= f;
-    update();
 }
 
 inline const float2_t &Transform::getOrigin() const
@@ -113,17 +99,13 @@ inline float Transform::getScale() const
     return scale_v;
 }
 
-inline void Transform::update()
+inline const float4x4_t Transform::getMatrix() const
 {
-    matrix = float4x4_t(1.0f);
+    float4x4_t matrix = float4x4_t(1.0f);
     matrix = glm::translate(matrix, float3_t(position, 0.0f));
     matrix = glm::rotate(matrix, glm::radians(rotation), float3_t(0.0f, 0.0f, 1.0f));
-    matrix = glm::translate(matrix, float3_t(-origin, 0.0f));
     matrix = glm::scale(matrix, float3_t(scale_v, scale_v, 1.0f));
-}
-
-inline const float4x4_t &Transform::getMatrix() const
-{
+    matrix = glm::translate(matrix, float3_t(-origin, 0.0f));
     return matrix;
 }
 } // namespace data
--- src/main.cc
+++ src/main.cc
@@ -120,12 +120,14 @@ int main(int argc, char **argv)
         image.getSize(width, height);
 
         sprite.size = float2_t(100.0f, 100.0f);
+
         sprite.texture.storage(width, height, GL_RGBA16F);
         sprite.texture.subImage(width, height, data::Image::TEXTURE_FORMAT, data::Image::TEXTURE_TYPE, image.getPixels());
         sprite.texture.setParameter(GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         sprite.texture.setParameter(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
         sprite.texture.setParameter(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         sprite.texture.setParameter(GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
         sprite.transform.setOrigin(float2_t(50.0f, 50.0f));
 
         std::vector<data::Transform> transforms;
@@ -142,28 +144,59 @@ int main(int argc, char **argv)
         sprite.transform.setPosition(float2_t(400.0f, 100.0f));
         transforms.push_back(sprite.transform);
 
+        // Sprite #4
+        transforms.push_back(sprite.transform);
+
+        // Sprite #5 = 2, 3 and 4 combined
+        transforms.push_back(sprite.transform);
+
         render::SpriteRenderer sprite_renderer(vidmode.width, vidmode.height);
         sprite_renderer.setView(data::View());
 
-        ui::init(window);
-        ui::LoggerOut logger_out;
-        ui::MenuBar menu_bar;
+        //ui::init(window);
+        //ui::LoggerOut logger_out;
+        //ui::MenuBar menu_bar;
+
+        util::Clock clock;
+        float sincos_angle = 0.0f;
+
+        util::Clock perf;
 
         while(!glfwWindowShouldClose(window)) {
+            const float frametime = clock.reset();
+
+            if(perf.getTime() >= 1.0f) {
+                util::log("momentary frametime: %f (%f FPS)", frametime, 1.0f / frametime);
+                perf.reset();
+            }
+
+            transforms[0].rotate(90.0f * frametime);
+            transforms[1].rotate(90.0f * frametime * cos(sincos_angle));
+            transforms[2].setScale(sin(sincos_angle));
+            transforms[3].setPosition(float2_t(550.0f, 100.0f) + 42.0f * float2_t(sin(sincos_angle), cos(sincos_angle)));
+            //transforms[3].move(50.0f * float2_t(sin(sincos_angle), cos(sincos_angle)));
+
+            transforms[4].rotate(90.0f * frametime * cos(sincos_angle));
+            transforms[4].setScale(sin(sincos_angle));
+            transforms[4].setPosition(float2_t(250.0f, 350.0f) + 75.0f * float2_t(sin(sincos_angle), cos(sincos_angle)));
+            //transforms[4].move(50.0f * float2_t(sin(sincos_angle), cos(sincos_angle)));
+
+            sincos_angle += frametime;
+
             glClear(GL_COLOR_BUFFER_BIT);
             glUseProgram(0);
 
             sprite_renderer.draw(transforms, sprite.texture, sprite.size);
 
-            const ImGuiIO &io = ui::beginFrame();
-            logger_out.draw(io);
-            menu_bar.draw(io);
-            ui::endFrame();
+            //const ImGuiIO &io = ui::beginFrame();
+            //logger_out.draw(io);
+            //menu_bar.draw(io);
+            //ui::endFrame();
 
-            if(menu_bar.file_exit) {
-                glfwSetWindowShouldClose(window, GLFW_TRUE);
-                menu_bar.file_exit = false;
-            }
+            //if(menu_bar.file_exit) {
+            //    glfwSetWindowShouldClose(window, GLFW_TRUE);
+            //    menu_bar.file_exit = false;
+            //}
 
             glBindProgramPipeline(0);
             glfwSwapBuffers(window);
